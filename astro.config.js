46fe3fd6a21753cd3c4a27ecaa8855f7bdf54fe1
Martynas Barzda <martynas@barzda.pro>
2024-05-13 20:49:37 +0300

    Generate sitemap and add robots.txt

--- astro.config.js
+++ astro.config.js
@@ -1,6 +1,8 @@
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://barzda.pro',
+  integrations: [sitemap({ lastmod: new Date() })],
 });
